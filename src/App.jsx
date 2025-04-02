38c367b52ed81e0e4f9344ef72f322139f27249f
Admin <Admin@DESKTOP-4NC3IP9>
2025-04-03 01:18:21 +0530

    Updated frontend with new API URLs and other changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; 
+import axios from "axios"
 import Navbar from "./components/navbar/Navbar";
 import TodoForm from "./components/todoForm/TodoForm";
 import TodoTable from "./components/todotable/TodoTable";
@@ -8,12 +9,45 @@ import TicTacToe from "./components/tictactoe/TicTacToe"
 const App = () => {
   const [todos, setTodos] = useState([]);
 
-  const addTodo = (newTodo) => {
-    setTodos((prev) => [...prev, newTodo]);
+  const addTodo = async (newTodo) => {
+    try {
+      const response = await axios.post('https://todo-backend-vzcb.onrender.com/api/todos', newTodo);
+      setTodos([...todos, response.data]); 
+    } catch (error) {
+      console.error('Error adding todo:', error);
+    }
   };
-  const handleDelete=(index)=>{
-    setTodos((prev)=>prev.filter((_,i)=> i!==index))
+
+  
+  const handleDelete= async(id)=>{
+    try{
+      await axios.delete(`https://todo-backend-vzcb.onrender.com/api/todos${id}`)
+      setTodos(todos.filter((todo)=>todo._id !==id));
+    }
+    catch(err){
+      console.log(err)
+    }
   }
+  
+  useEffect(()=>{
+    
+    const fetchTodos = async () =>{
+   
+      try{
+       const response= await axios.get("https://todo-backend-vzcb.onrender.com/api/todos")
+         setTodos(response.data)
+      }
+       catch(error){
+           console.log(error)
+       }
+    }
+
+   fetchTodos();
+
+  },[])
+
+
+
 
 
 
